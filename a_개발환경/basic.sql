376793e8e33b89fb6e67f785be3b1c969ad3f9aa
tndals11 <136726862+tndals11@users.noreply.github.com>
2025-07-28 12:15:31 +0900

    20250728_DBMS_Setting&Basic-Skills
    DBMS에 대해서 셋팅하는 법과 기초를 학습

---
+++ a_개발환경/basic.sql
@@ -0,0 +1,50 @@
+-- MySQL Workbench 환경설정 (basic.sql)
+
+-- 1. 주석
+-- 1) 단축키: ctrl + /
+
+-- 2) 한 줄 주석: 하이픈 2개 OR 샵 1개
+
+# 안녕하세요 주석입니다 :)
+-- 이것도 주석입니다 :)  
+
+-- 3) 여러 줄 주석 /**/
+
+/*
+	줄 수에 관계 없이
+    주석 처리 가능
+*/
+
+-- 이 쿼리는 모든 데이터베이스를 보여줌
+SHOW DATABASES;
+
+/*
+	이 쿼리는 
+    모든 데이터베이스를 보여줌
+*/
+SHOW DATABASES;
+
+-- === 2. 글자 크기 변경 ===
+-- 1) ctrl + 마우스 휠 위/아래
+
+-- 2) Edit > Prefences > FoNTS & COLORS
+-- Font명 클자크기값 (Consolas 26)
+
+-- === 3. 명령어 대소문자 설정 === 
+-- : 문법의 대소문자 구분 X
+-- >> 일관성 있는 작성을 권장 !
+-- 1) 명령어(문법)는 대문자
+-- 2) 테이블 & 컬럼명은 소문자 
+
+show databases; -- MySQL Workbench는 소문자 자동완성 기본
+SHOW DATABASES; 
+
+-- 대문자 변환 단축키: ctrl + shift + u
+-- Edit > Format > UPCASE(대문자) / lowercase(소문자) keywords
+
+-- === 4. 단축키 ===
+# 파일 저장: ctrl + s 
+
+
+
+
